7a36738015c5854eca7d7e696c69c6bbbe1e7ef3
paul.tangbaoan <paul.tangbaoan@inspiretec.com>
2021-05-03 14:17:23 +0800

    replaced null href values for netlify to deploy.

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light nav-bg">
         <div className="container">
-          <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"></img></a>
+          <a className="navbar-brand" href="/#"><img className="logo" src={logo} alt="logo"></img></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" 
           data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
           aria-expanded="false" aria-label="Toggle navigation">          
@@ -16,19 +16,19 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home</a>
+                <a className="nav-link" href="/#">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
+                <a className="nav-link" href="/#">About</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Services</a>
+                <a className="nav-link" href="/#">Services</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">how work</a>
+                <a className="nav-link" href="/#">how work</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">portfolio</a>
+                <a className="nav-link" href="/#">portfolio</a>
               </li>
             </ul>
           </div>
