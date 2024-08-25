f471ab724228162b5c439778e8430bb64ab1aa29
paul <paulchristian_tangbaoan@yahoo.com>
2024-08-25 17:48:05 +0800

    update project data

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,56 +1,76 @@
 let Projectdata = [
     {
       id: 1,
-      title: "Lead Full Stack .NET Developer",
+      title: "Integration Developer",
       location: "UK, London",
       description:
-        "Lead developer for a newly introduce features for cruises and ferries as well the hotel accommodations. Conduct reviews of requirements and specifications and contribute to code and design reviews.",
-      buttonText: "View Demo",
-      date: "March 2021 - present",
+        "Involves integrating legacy systems from classic ASP into a modern .NET architecture, developing scalable applications to enhance and support these older systems. Additionally, I restructure and optimize database schemas to improve data allocation and normalization, introducing new layouts and structures for more efficient data transfer.",
+      buttonText: "Event Connections",
+      date: "Aug 2023 - Present",
       icon: "work"
     },
     {
       id: 2,
-      title: "Senior Software Engineer",
-      location: "Taguig, PH",
+      title: ".NET Developer",
+      location: "USA",
       description:
-        "Collaborate with internal teams and onshore people to produce software design and architecture. Refractor and debug existing systems. Developed innovation that will help on the project process improvements. Automation",
-      buttonText: "View Demo",
-      date: "Feb 2019 - Feb 2021",
+      "I focus on backend development, where I build scalable microservices and APIs using GraphQL. Alongside my backend work, I have some experience with frontend development using Angular. I’m currently working on a product that integrates CRM, accounting, and field service management. One of the key features I developed is a job booking system with real-time timezone conversion, which improves global scheduling capabilities.",
+      buttonText: "Scorpion",
+      date: "April 2021 - July 2023",
       icon: "work"
     },
     {
       id: 3,
-      title: ".NET Developer Consultant",
-      location: "Makati, PH",
+      title: "Lead Full Stack .NET Developer",
+      location: "UK, London",
       description:
-        "Integrated UI into newest components, Refractor and Developed highly maintainable stored procedures. Test and deploy applications and systems. perform complex programming, designing and analysis to meet business requirements.",
-      buttonText: "View Demo",
-      date: "Feb 2017 - Feb 2019",
+        "Lead developer for a newly introduce features for cruises and ferries as well the hotel accommodations. Conduct reviews of requirements and specifications and contribute to code and design reviews.",
+      buttonText: "Inspiretec",
+      date: "March 2021 - April 2021",
       icon: "work"
     },
     {
       id: 4,
-      title: "Programmer Analyst",
-      location: "Makati, PH",
+      title: "Senior Software Engineer",
+      location: "Taguig, PH",
       description:
-        "Involve in migration and integration of bank retail e-banking into newest version. Part of the implementation of UI/UX Desgin using FEBA Framework. Developed security pages and promotional page.",
-      buttonText: "View Demo",
-      date: "Oct 2014 - Dec 2017",
+        "Collaborate with internal teams and onshore people to produce software design and architecture. Refractor and debug existing systems. Developed innovation that will help on the project process improvements. Automation",
+      buttonText: "Accenture",
+      date: "Feb 2019 - Feb 2021",
       icon: "work"
     },
     {
       id: 5,
-      title: "Programmer",
-      location: "Caloocan, PH",
+      title: ".NET Developer Consultant",
+      location: "Makati, PH",
       description:
-        "Developed Automation using C# and Selenium to help the Quality Assurance for daily basis.",
-      buttonText: "View Demo",
-      date: "March 2014 - Aug 2014",
+        "Integrated UI into newest components, Refractor and Developed highly maintainable stored procedures. Test and deploy applications and systems. perform complex programming, designing and analysis to meet business requirements.",
+      buttonText: "BDO Unibank",
+      date: "Feb 2017 - Feb 2019",
       icon: "work"
     },
     {
       id: 6,
+      title: "Programmer Analyst",
+      location: "Makati, PH",
+      description:
+        "Involve in migration and integration of bank retail e-banking into newest version. Part of the implementation of UI/UX Desgin using FEBA Framework. Developed security pages and promotional page.",
+      buttonText: "Chinabank",
+      date: "Oct 2014 - Dec 2017",
+      icon: "work"
+    },
+    {
+      id: 7,
+      title: "Programmer",
+      location: "Caloocan, PH",
+      description:
+        "Developed Automation using C# and Selenium to help the Quality Assurance for daily basis.",
+      buttonText: "Lee System Ventures Inc.",
+      date: "March 2014 - Aug 2014",
+      icon: "work"
+    },
+    {
+      id: 8,
       title: "University of the East Manila",
       location: "Manila, PH",
       description:
