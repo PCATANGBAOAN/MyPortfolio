944772610d89d24541374c9ed11e302cb0993207
paul <paulchristian_tangbaoan@yahoo.com>
2024-08-25 17:55:39 +0800

    update headers

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import logo from '../assets/img/icons/paullogo4.png';
+import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-scroll';
@@ -7,76 +6,82 @@ import { Link } from 'react-scroll';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light nav-bg">
-        <div className="container">
-          <a className="navbar-brand" href="/#"><img className="logo" src={logo} alt="logo"></img></a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" 
-          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-          aria-expanded="false" aria-label="Toggle navigation">          
-            <FontAwesomeIcon icon={faBars} style={{color:'white'}}/>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-             
-              <Link
-                activeClass="active"
-                to="Home"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-              >Home
-              </Link>
-              </li>
-              <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="About"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-              >About
-              </Link>
-              </li>
-              <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="Project"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-              >Timeline
-              </Link>
-              </li>
-              <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="myprojects"
-                spy={true}
-                smooth={true}
-                offset={-120}
-                duration={500}
-              >Projects
-              </Link>
-              </li>
-              <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="Footer"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-              >LET'S TALK
-              </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    )
+            <div className="container">
+                {/* Removed logo image here */}
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={faBars} style={{ color: 'white' }} />
+                </button>
+
+                <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="Home"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="About"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="Project"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                            >
+                                Timeline
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="myprojects"
+                                spy={true}
+                                smooth={true}
+                                offset={-120}
+                                duration={500}
+                            >
+                                Projects
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                activeClass="active"
+                                to="Footer"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                            >
+                                LET'S TALK
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
